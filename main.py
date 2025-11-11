5ebf2cc5f4c4a7199887b61534db025c33ac6bf1
sxsorensen <sxsorensen2@students.everettcc.edu>
2025-11-11 14:50:34 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+#MORTGAGE CALCULATOR
+
+#Import functions and dictionary
+#Welcome the user to system
+#Ask the user for terms: principal, interest rate, years
+#Calculate APR %, monthly mortgage payment (P&I)
+#Create payment schedule
+
+
+from mortgage import loan
+
+#REQUEST DATA FROM USER
+principal = float(input("How much is the princiapl of your loan? "))
+irate = float(input("What is the interest rate? "))
+term = integar(input("What is the term of the loan in years? "))
+table = input("Would you like a schedule of payments? Y/N "
+
+loan = Loan(principal, irate, term)
